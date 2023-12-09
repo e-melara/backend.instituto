6ca161c86de955f3fe3d90a32f3cb9ebec63d685
Consultoria 01 SV <consultoria01sv@gmail.com>
2023-12-09 12:32:41 -0600

    HTML permission

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link
@@ -8,8 +9,8 @@
       href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
     />
     <title>PORTAL</title>
-    <script type="module" crossorigin src="{{ asset('assets/index-e0048f3a.js') }}"></script>
-    <link rel="stylesheet" href="{{ asset('assets/index-49376683.css') }}" />
+    <script type="module" crossorigin src="/assets/index-e0048f3a.js"></script>
+    <link rel="stylesheet" href="/assets/index-49376683.css" />
   </head>
   <body>
     <div id="app"></div>
