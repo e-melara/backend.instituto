fd06f6d62a2ca564492093a41fb3378163dcb3e5
Consultoria 01 SV <consultoria01sv@gmail.com>
2023-12-12 05:55:18 -0600

    DESPLIGUE 12/12/2023

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link
@@ -9,10 +8,11 @@
       href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
     />
     <title>PORTAL</title>
-    <script type="module" crossorigin src="/assets/index-e0048f3a.js"></script>
-    <link rel="stylesheet" href="/assets/index-49376683.css" />
+    <script type="module" crossorigin src="/assets/index-d66447e3.js"></script>
+    <link rel="stylesheet" href="/assets/index-c4fc2e68.css">
   </head>
   <body>
     <div id="app"></div>
+    
   </body>
 </html>
