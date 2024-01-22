38dfe8c815b21b31ada0d5072dcb35bd341a3635
Consultoria 01 SV <consultoria01sv@gmail.com>
2024-01-21 22:05:33 -0600

    Mejoras 21/01/24

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,8 @@
       href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
     />
     <title>PORTAL</title>
-    <script type="module" crossorigin src="/assets/index-39ca847a.js"></script>
-    <link rel="stylesheet" href="/assets/index-978e80b3.css">
+    <script type="module" crossorigin src="/assets/index-507cb3d4.js"></script>
+    <link rel="stylesheet" href="/assets/index-75a2945b.css">
   </head>
   <body>
     <div id="app"></div>
