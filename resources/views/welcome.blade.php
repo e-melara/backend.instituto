a5baa211a672ba1c1d3e7ad06088b1da3e59e8f9
Edwin Melara <edwin.melara.landaverde@gmail.com>
2024-09-18 21:52:38 -0600

    18/09/24 v2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,8 @@
       href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
     />
     <title>PORTAL</title>
-    <script type="module" crossorigin src="/assets/index-5bad5049.js"></script>
-    <link rel="stylesheet" href="/assets/index-aa485d64.css">
+    <script type="module" crossorigin src="/assets/index-b887e278.js"></script>
+    <link rel="stylesheet" href="/assets/index-a31590d4.css">
   </head>
   <body>
     <div id="app"></div>
