194fc5cda361f89769f6d26784b5b394a9ce1c12
Edwin Melara <edwin.melara@incaf.gob.sv>
2024-11-04 08:53:39 -0600

    04/nov/2024

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,8 @@
       href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
     />
     <title>PORTAL</title>
-    <script type="module" crossorigin src="/assets/index-d9bdaffa.js"></script>
-    <link rel="stylesheet" href="/assets/index-ecc8b336.css">
+    <script type="module" crossorigin src="/assets/index-e95228c0.js"></script>
+    <link rel="stylesheet" href="/assets/index-3697e156.css">
   </head>
   <body>
     <div id="app"></div>
