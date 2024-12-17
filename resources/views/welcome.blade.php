47f92634e4bd7ecb558a64d06626f51c9948c33e
Edwin Melara <edwin.melara.landaverde@gmail.com>
2024-12-16 21:27:15 -0600

    Dev 16dic

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
     />
     <title>PORTAL</title>
-    <script type="module" crossorigin src="/assets/index-41095057.js"></script>
-    <link rel="stylesheet" href="/assets/index-2fc33431.css">
-</head>
-<body>
-<div id="app"></div>
-</body>
+    <script type="module" crossorigin src="/assets/index-ad459ec4.js"></script>
+    <link rel="stylesheet" href="/assets/index-29c71f3c.css">
+  </head>
+  <body>
+    <div id="app"></div>
+  </body>
 </html>
